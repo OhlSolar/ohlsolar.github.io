2ae83c78510e764427edd4d1ab2d5b9720d097fc
VirusKA <devaprise@outlook.com>
2016-09-02 12:59:30 -0400

    updates

---
+++ js/main.js
@@ -0,0 +1,103 @@
+jQuery(document).ready(function($){
+	//cache some jQuery objects
+	var modalTrigger = $('.cd-modal-trigger'),
+		transitionLayer = $('.cd-transition-layer'),
+		transitionBackground = transitionLayer.children(),
+		modalWindow = $('.window');
+
+	var frameProportion = 1.78, //png frame aspect ratio
+		frames = transitionLayer.data('frame'), //number of png frames
+		resize = false;
+
+	//set transitionBackground dimentions
+	setLayerDimensions();
+	$(window).on('resize', function(){
+		if( !resize ) {
+			resize = true;
+			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+		}
+	});
+
+	//open modal window
+	modalTrigger.on('click', function(event){	
+		event.preventDefault();
+		var modalId = $(event.target).attr('href');
+		transitionLayer.addClass('visible opening');
+		var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 800;
+		setTimeout(function(){
+			modalWindow.filter(modalId).addClass('visible');
+			transitionLayer.removeClass('opening');
+		}, delay);
+	});
+
+	//close modal window
+	modalWindow.on('click', '.modal-close', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('closing');
+		modalWindow.removeClass('visible');
+		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+			transitionLayer.removeClass('closing opening visible');
+			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+		});
+	});
+
+	function setLayerDimensions() {
+		var windowWidth = $(window).width(),
+			windowHeight = $(window).height(),
+			layerHeight, layerWidth;
+
+		if( windowWidth/windowHeight > frameProportion ) {
+			layerWidth = windowWidth;
+			layerHeight = layerWidth/frameProportion;
+		} else {
+			layerHeight = windowHeight*1.2;
+			layerWidth = layerHeight*frameProportion;
+		}
+
+		transitionBackground.css({
+			'width': layerWidth*frames+'px',
+			'height': layerHeight+'px',
+		});
+
+		resize = false;
+	}
+    //move nav element position according to window width
+	moveNavigation();
+	$(window).on('resize', function(){
+		(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+	});
+
+	//mobile version - open/close navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		if($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
+		
+		$('header').toggleClass('nav-is-visible');
+		$('.main-nav').toggleClass('nav-is-visible');
+		$('.main-content').toggleClass('nav-is-visible');
+	});
+
+	//mobile version - go back to main navigation
+	$('.go-back').on('click', function(event){
+		event.preventDefault();
+		$('.main-nav').removeClass('moves-out');
+	});
+    function moveNavigation(){
+		var navigation = $('.navigation');
+  		var screenSize = checkWindowWidth();
+        if ( screenSize ) {
+        	//desktop screen - insert inside header element
+			navigation.detach();
+			navigation.insertBefore('.cd-nav-trigger');
+		} else {
+			//mobile screen - insert after .cd-main-content element
+			navigation.detach();
+			navigation.insertAfter('.main-content');
+		}
+	}
+
+	function checkWindowWidth() {
+		var mq = window.getComputedStyle(document.querySelector('header'), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
+		return ( mq == 'mobile' ) ? false : true;
+	}
+});
